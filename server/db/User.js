818b704fe9d2582e38072e07140c58c7819891ca
Justin Mattos <justin.mattos@nyu.edu>
2021-04-25 23:26:14 -0400

    added User model

---
+++ server/db/User.js
@@ -0,0 +1,62 @@
+const { DataTypes, Model } = require('sequelize');
+const db = require('./db');
+
+class User extends Model {}
+User.init(
+  {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      //build in hashing at some point
+      validate: {},
+    },
+    phoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    fullName: {
+      type: DataTypes.VIRTUAL,
+      get: () => {
+        return `${this.firstName} ${this.lastName}`;
+      },
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      validate: {
+        isBool(value) {
+          if (value !== true || value !== false) {
+            throw new Error('Only boolean values are allowed');
+          }
+        },
+      },
+    },
+  },
+  { sequelize: db, modelName: 'user' }
+);
+
+module.exports = User;
