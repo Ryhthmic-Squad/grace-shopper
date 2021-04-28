afd7d05656a6e0f8cea92a75bce6492e88e63eed
Justin Mattos <justin.mattos@nyu.edu>
2021-04-28 18:12:31 -0400

    Added Cart-related associations to server/db/index.js

--- server/db/index.js
+++ server/db/index.js
@@ -4,12 +4,24 @@ const db = require('./db');
 const User = require('./User');
 const Product = require('./Product');
 const Address = require('./Address');
+const Cart = require('./Cart');
+const CartProduct = require('./CartProduct');
 
 //Set up relations for models here
 //e.g. Product.belongsTo(Room);
+
+//Users can only have a single cart associated with their account
+User.hasOne(Cart);
+Cart.belongsTo(User);
+
+//Users can have many addresses associated with their account
 User.hasMany(Address);
 Address.belongsTo(User);
 
+// CartProduct is a connection table that connects a product to an order and keeps a quantity:
+Cart.belongsToMany(Product, { through: CartProduct });
+Product.belongsToMany(Cart, { through: CartProduct });
+
 //export the database connection and models from this file
 
-module.exports = { db, models: { User, Product, Address } };
+module.exports = { db, models: { User, Product, Address, Cart, CartProduct } };
