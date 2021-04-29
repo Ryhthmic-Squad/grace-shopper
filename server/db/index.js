80f01d5a92bc90dca97f7cfc7fd5bd72bfee8cb0
Justin Mattos <justin.mattos@nyu.edu>
2021-04-28 21:52:31 -0400

    added Review model plus associations & tests

--- server/db/index.js
+++ server/db/index.js
@@ -2,23 +2,24 @@ const db = require('./db');
 //Import all models here
 //e.g. const Product = require('./Product');
 const User = require('./User');
-const Product = require('./Product');
 const Address = require('./Address');
 const Order = require('./Order');
 const OrderProduct = require('./OrderProduct');
 const Cart = require('./Cart');
 const CartProduct = require('./CartProduct');
+const Review = require('./Review');
+const Product = require('./Product');
 
 //Set up relations for models here
 //e.g. Product.belongsTo(Room);
 
-// Users can have many orders associated with their account
-User.hasMany(Order);
-Order.belongsTo(User);
-
 // Users can have many addresses associated with their account
 User.hasMany(Address);
 Address.belongsTo(User);
+
+// Users can have many orders associated with their account
+User.hasMany(Order);
+Order.belongsTo(User);
 
 // OrderProduct is a connection table that connects a product to an order and keeps a quantity:
 Order.belongsToMany(Product, { through: OrderProduct });
@@ -36,8 +37,23 @@ Address.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartProduct });
 Product.belongsToMany(Cart, { through: CartProduct });
 
+// Users can have many reviews, and products can have many reviews
+User.hasMany(Review);
+Product.hasMany(Review);
+Review.belongsTo(User);
+Review.belongsTo(Product);
+
 //export the database connection and models from this file
 module.exports = {
   db,
-  models: { User, Address, Order, OrderProduct, Cart, CartProduct, Product },
+  models: {
+    User,
+    Address,
+    Order,
+    OrderProduct,
+    Cart,
+    CartProduct,
+    Review,
+    Product,
+  },
 };
