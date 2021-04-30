9c759eaa952b2f374085ed80476542bf962b603e
mmartin2755@gmail.com <mmartin2755@gmail.com>
2021-04-29 22:46:17 -0400

    fixed describe blocks

--- test/Models/Product.spec.js
+++ test/Models/Product.spec.js
@@ -30,111 +30,112 @@ describe('Product Model', () => {
   afterEach(async () => {
     await newProduct.destroy();
   });
-});
-describe('Attribute: name', () => {
-  it('has a name', () => {
-    expect(newProduct.name).to.be.ok;
-  });
-  it('name cannot be empty', async () => {
-    newProduct.name = '';
-    try {
-      await newProduct.save();
-      //above should throw an error, so below should not run
-      expect(true).to.equal(false);
-    } catch (err) {
-      expect(err instanceof ValidationError).to.equal(true);
-    }
-  });
-  it('name cannot be null', async () => {
-    newProduct.name = null;
-    try {
-      await newProduct.save();
-      //above should throw an error, so below should not run
-      expect(true).to.equal(false);
-    } catch (err) {
-      expect(err instanceof ValidationError).to.equal(true);
-    }
-  });
-});
 
-describe('Attribute: inventory', () => {
-  it('has inventory', () => {
-    expect(newProduct.inventory).to.be.ok;
+  describe('Attribute: name', () => {
+    it('has a name', () => {
+      expect(newProduct.name).to.be.ok;
+    });
+    it('name cannot be empty', async () => {
+      newProduct.name = '';
+      try {
+        await newProduct.save();
+        //above should throw an error, so below should not run
+        expect(true).to.equal(false);
+      } catch (err) {
+        expect(err instanceof ValidationError).to.equal(true);
+      }
+    });
+    it('name cannot be null', async () => {
+      newProduct.name = null;
+      try {
+        await newProduct.save();
+        //above should throw an error, so below should not run
+        expect(true).to.equal(false);
+      } catch (err) {
+        expect(err instanceof ValidationError).to.equal(true);
+      }
+    });
   });
-  it('inventory cannot be empty', async () => {
-    newProduct.inventory = '';
-    try {
-      await newProduct.save();
-      //above should throw an error, so below should not run
-      expect(true).to.equal(false);
-    } catch (err) {
-      expect(err instanceof ValidationError).to.equal(true);
-    }
-  });
-});
 
-describe('Attribute: imageUrl', () => {
-  it('has a name attribute', () => {
-    expect(newProduct.imageUrl).to.be.ok;
+  describe('Attribute: inventory', () => {
+    it('has inventory', () => {
+      expect(newProduct.inventory).to.be.ok;
+    });
+    it('inventory cannot be empty', async () => {
+      newProduct.inventory = '';
+      try {
+        await newProduct.save();
+        //above should throw an error, so below should not run
+        expect(true).to.equal(false);
+      } catch (err) {
+        expect(err instanceof ValidationError).to.equal(true);
+      }
+    });
   });
-  it('only allows valid imageUrls', async () => {
-    newProduct.imageUrl = 'a fake website';
-    try {
-      await newProduct.save();
-      //above should throw an error, so below should not run
-      expect(true).to.equal(false);
-    } catch (err) {
-      expect(err instanceof ValidationError).to.equal(true);
-    }
-  });
-});
 
-describe('Attribute: description', () => {
-  it('has a name attribute', () => {
-    expect(newProduct.description).to.be.ok;
+  describe('Attribute: imageUrl', () => {
+    it('has a name attribute', () => {
+      expect(newProduct.imageUrl).to.be.ok;
+    });
+    it('only allows valid imageUrls', async () => {
+      newProduct.imageUrl = 'a fake website';
+      try {
+        await newProduct.save();
+        //above should throw an error, so below should not run
+        expect(true).to.equal(false);
+      } catch (err) {
+        expect(err instanceof ValidationError).to.equal(true);
+      }
+    });
   });
-  it('description cannot be empty', async () => {
-    newProduct.description = '';
-    try {
-      await newProduct.save();
-      //above should throw an error, so below should not run
-      expect(true).to.equal(false);
-    } catch (err) {
-      expect(err instanceof ValidationError).to.equal(true);
-    }
-  });
-});
 
-describe('Attribute: dimensions', () => {
-  it('has height, width, and optional depth', () => {
-    expect(newProduct.height).to.be.ok;
-    expect(newProduct.width).to.be.ok;
-    expect(newProduct.depth).to.be.ok;
+  describe('Attribute: description', () => {
+    it('has a name attribute', () => {
+      expect(newProduct.description).to.be.ok;
+    });
+    it('description cannot be empty', async () => {
+      newProduct.description = '';
+      try {
+        await newProduct.save();
+        //above should throw an error, so below should not run
+        expect(true).to.equal(false);
+      } catch (err) {
+        expect(err instanceof ValidationError).to.equal(true);
+      }
+    });
   });
-});
 
-describe('Attribute: description', () => {
-  it('has a name attribute', () => {
-    expect(newProduct.description).to.be.ok;
+  describe('Attribute: dimensions', () => {
+    it('has height, width, and optional depth', () => {
+      expect(newProduct.height).to.be.ok;
+      expect(newProduct.width).to.be.ok;
+      expect(newProduct.depth).to.be.ok;
+    });
   });
-  it('description cannot be empty', async () => {
-    newProduct.description = '';
-    try {
-      await newProduct.save();
-      //above should throw an error, so below should not run
-      expect(true).to.equal(false);
-    } catch (err) {
-      expect(err instanceof ValidationError).to.equal(true);
-    }
+
+  describe('Attribute: description', () => {
+    it('has a name attribute', () => {
+      expect(newProduct.description).to.be.ok;
+    });
+    it('description cannot be empty', async () => {
+      newProduct.description = '';
+      try {
+        await newProduct.save();
+        //above should throw an error, so below should not run
+        expect(true).to.equal(false);
+      } catch (err) {
+        expect(err instanceof ValidationError).to.equal(true);
+      }
+    });
   });
-});
-describe('Attribute: style', () => {
-  it('has a style', () => {
-    expect(newProduct.style).to.be.ok;
+  describe('Attribute: style', () => {
+    it('has a style', () => {
+      expect(newProduct.style).to.be.ok;
+    });
   });
-});
-describe('Attribute: type', () => {
-  it('has a type', () => {
-    expect(newProduct.type).to.be.ok;
+  describe('Attribute: type', () => {
+    it('has a type', () => {
+      expect(newProduct.type).to.be.ok;
+    });
   });
 });
