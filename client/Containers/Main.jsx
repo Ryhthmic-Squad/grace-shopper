84b0ad3469b9fdf6d439f82daa7b6b3b36528ff4
mmartin2755@gmail.com <mmartin2755@gmail.com>
2021-05-03 22:51:05 -0400

    main page partially working

--- client/Containers/Main.jsx
+++ client/Containers/Main.jsx
@@ -12,25 +12,27 @@ import {
 import AllProducts from './AllProducts';
 import MainNav from './MainNav';
 import FeaturedButton from '../components/styles/FeaturedButton';
+import LandingPage from '../components/styles/LandingPage.js';
 
 class Main extends Component {
   render() {
     const { fetchProductList } = this.props;
     return (
       <Router>
-        <div>
-          <Route component={MainNav} />
-          <Route component={AllProducts} path="/products" exact />
-          <Route component={UserDashboard} path="/login" exact />
-          <hr />
-          <Link to="/products">
-            <FeaturedButton> Shop All Furniture </FeaturedButton>
-          </Link>
-
-          <hr />
-          <hr className="heavy" />
-          <TestProductList />
-        </div>
+        <MainNav />
+        <LandingPage>
+          <div>
+            {/* <Route component={MainNav} /> */}
+            <Route component={AllProducts} path="/products" exact />
+            <Route component={UserDashboard} path="/login" exact />
+            <hr />
+            <Link to="/products">
+              <FeaturedButton> Shop All Furniture </FeaturedButton>
+            </Link>
+            <hr />
+            <TestProductList />
+          </div>
+        </LandingPage>
       </Router>
     );
   }
---
+++ client/components/styles/LandingPage.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const LandingPage = styled.div`
+  background-image: url('https://www.studioilse.com/wp-content/uploads/2020/04/2-kitchen-CF029603-1050x766.jpg');
+`;
+
+export default LandingPage;
