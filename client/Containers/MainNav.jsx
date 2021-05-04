067a41a0912e4f7d2465b794e4bffe8e5c5a8ced
Vikki Chan <vikkiwchan@gmail.com>
2021-05-04 16:14:29 -0500

    removed unused import

--- client/Containers/MainNav.jsx
+++ client/Containers/MainNav.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Router, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { MainNavDiv } from '../components/styles/MainNavDiv';
 
 class MainNav extends Component {
